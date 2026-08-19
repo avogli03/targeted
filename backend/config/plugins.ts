bdc9719c66ef9b8e3a6dec89a758ca8b1de2bccc
AV <ardit.vogli03@gmail.com>
2026-08-19 12:53:53 +0200

    Add Cloudinary upload provider support for persistent media on Render

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,5 +1,21 @@
-export default {
+export default ({ env }) => ({
   i18n: {
     enabled: true
+  },
+  upload: {
+    config: env("CLOUDINARY_NAME")
+      ? {
+          provider: "cloudinary",
+          providerOptions: {
+            cloud_name: env("CLOUDINARY_NAME"),
+            api_key: env("CLOUDINARY_KEY"),
+            api_secret: env("CLOUDINARY_SECRET")
+          },
+          actionOptions: {
+            upload: {},
+            delete: {}
+          }
+        }
+      : undefined
   }
-};
+});
