e1211c379aff3e87ace19577bdd2aeaf5e283cb5
AV <ardit.vogli03@gmail.com>
2026-08-19 12:55:36 +0200

    Revert "Add Cloudinary upload provider support for persistent media on Render"
    This reverts commit bdc9719c66ef9b8e3a6dec89a758ca8b1de2bccc.

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,21 +1,5 @@
-export default ({ env }) => ({
+export default {
   i18n: {
     enabled: true
-  },
-  upload: {
-    config: env("CLOUDINARY_NAME")
-      ? {
-          provider: "cloudinary",
-          providerOptions: {
-            cloud_name: env("CLOUDINARY_NAME"),
-            api_key: env("CLOUDINARY_KEY"),
-            api_secret: env("CLOUDINARY_SECRET")
-          },
-          actionOptions: {
-            upload: {},
-            delete: {}
-          }
-        }
-      : undefined
   }
-});
+};
